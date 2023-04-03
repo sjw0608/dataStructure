cbf88ecd9345413196c506bc5856fa173398d8a0
wb_songjiangwei <wb_songjiangwei@meituan.com>
2023-04-03 18:03:28 +0800

    AVL自平衡二叉树

--- src/tree/tree.js
+++ src/tree/tree.js
@@ -4,6 +4,14 @@ const Compare = {
     EQUALS: 0
 }
 
+const BalanceFactor = {
+    UNBALANCED_RIGHT: 1,
+    SLIGHTLY_UNBALANCED_RIGHT: 2,
+    BALANCED: 3,
+    SLIGHTLY_UNBALANCED_LEFT: 4,
+    UNBALANCED_LEFT: 5
+};
+
 function defaultCompare(a, b) {
     if (a === b) {
         return Compare.EQUALS;
@@ -16,6 +24,9 @@ class Node {
         this.key = key; // 节点值
         this.left = null; // 左侧节点
         this.right = null; // 右侧节点
+    }
+    toString() {
+        return `${this.key}`;
     }
 }
 
@@ -208,6 +219,142 @@ class BinarySearchTree {
 class AVLTree extends BinarySearchTree {
     constructor(compareFn = defaultCompare) {
         super(compareFn);
+        this.compareFn = compareFn;
+        this.root = null;
     }
+    // 计算节点的高度
+    getNodeHeight(node) {
+        if (node == null) return -1;
+        return Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) + 1;
+    }
+    // 计算一个节点的平衡因子
+    getBalanceFactor(node) {
+        const heightDifference = this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
+        switch (heightDifference) {
+            case -2:
+                return BalanceFactor.UNBALANCED_RIGHT;
+            case -1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
+            case 1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
+            case 2:
+                return BalanceFactor.UNBALANCED_LEFT;
+            default:
+                return BalanceFactor.BALANCED;
+        }
+    }
+    /**
+     * 左->左：向右的单旋转
+     * 右->右：向左的单旋转
+     * 左->右：向右的双旋转（先LL旋转，再RR旋转）
+     * 右->左：向左的双旋转（先RR旋转，再LL旋转）
+     */
+    /**
+     * 左->左: 向右的单旋转
+     * 这种情况出现于节点的左侧子节点的高度大于右侧子节点的高度时，并且左侧子节点也是平衡或左侧较重的
+     *
+     *       b                           a
+     *      / \                         / \
+     *     a   e -> rotationLL(b) ->   c   b
+     *    / \                             / \
+     *   c   d                           d   e
+     *
+     * @param node Node<T>
+     */
+    rotationLL(node) {
+        const tmp = node.left;
+        node.left = tmp.right;
+        tmp.right = node;
+        return tmp;
+    }
+    /**
+     * 右->右: 向左的单旋转
+     * 它出现于右侧子节点的高度大于左侧子节点的高度，并且右侧子节点也是平衡或右侧较重的
+     *
+     *     a                              b
+     *    / \                            / \
+     *   c   b   -> rotationRR(a) ->    a   e
+     *      / \                        / \
+     *     d   e                      c   d
+     *
+     * @param node Node<T>
+     */
+    rotationRR(node) {
+        const tmp = node.right;
+        node.right = tmp.left;
+        tmp.left = node;
+        return tmp;
+    }
+    /**
+     * 左->右：向右的双旋转（先LL旋转，再RR旋转）
+     * 这种情况出现于左侧子节点的高度大于右侧子节点的高度，并且左侧子节点右侧较重
+     * @param {*} node Node<T>
+     */
+    rotationLR(node) {
+        node.left = this.rotationRR(node.left);
+        return this.rotationLL(node);
+    }
+    /**
+     * 右->左：向左的双旋转（先RR旋转，再LL旋转）
+     * 这种情况出现于右侧子节点的高度大于左侧子节点的高度，并且右侧子节点左侧较重
+     * @param {*} node Node<T>
+     */
+    rotationRL(node) {
+        node.right = this.rotationLL(node.right);
+        return this.rotationRR(node);
+    }
+    insert(key) {
+        this.root = this.insertNode(this.root, key);
+    }
+    insertNode(node, key) {
+        if (node == null) return new Node(key);
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            node.left = this.insertNode(node.left, key);
+        } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            node.right = this.insertNode(node.right, key);
+        } else {
+            return node;
+        }
+        const balanceFactor = this.getBalanceFactor(node);
+        if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+            if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+                node = this.rotationLL(node);
+            } else {
+                node = this.rotationLR(node);
+            }
+        }
+        if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+            if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+                node = this.rotationRR(node);
+            } else {
+                node = this.rotationRL(node);
+            }
+        }
+        return node;
+    }
+    removeNode(node, key) {
+        node = super.removeNode(node, key);
+        if (node == null) return node;
+        const balanceFactor = this.getBalanceFactor(node);
+        if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+            const balanceFactorLeft = this.getBalanceFactor(node.left);
+            if (balanceFactorLeft === BalanceFactor.BALANCED || balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT) {
+                return this.rotationLL(node);
+            }
+            if (balanceFactorLeft === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT) {
+                return this.rotationLR(node.left);
+            }
+        }
 
+        if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+            const balanceFactorRight = this.getBalanceFactor(node.left);
+            if (balanceFactorRight === BalanceFactor.BALANCED || balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT) {
+                return this.rotationRR(node);
+            }
+            if (balanceFactorRight === BalanceFactor.SLIGHTLY_UNBALANCED_LEFT) {
+                return this.rotationRL(node.right);
+            }
+        }
+        return node;
+    }
 }
