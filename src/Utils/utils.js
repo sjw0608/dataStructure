e1e5e55afa5fa060ec2d2341115dd247c9c94721
wb_songjiangwei <wb_songjiangwei@meituan.com>
2023-05-06 10:26:33 +0800

    公共方法

---
+++ src/Utils/utils.js
@@ -0,0 +1,187 @@
+
+/**
+ * 
+ * @param {*} str 校验数据
+ * @param {email, mobile, phone, url,...} type 校验类型
+ * @param {boolean} isCardStrict
+ * @returns Boolean
+ */
+const checkStr = (str, type, isCardStrict = false) => {
+    switch (type) {
+        case 'phone':   //手机号码
+            return /^1[3|4|5|6|7|8|9][0-9]{9}$/.test(str);
+        case 'tel':     //座机
+            return /^(0\d{2,3}-\d{7,8})(-\d{1,4})?$/.test(str);
+        case 'card':    //身份证
+            if (isCardStrict) {
+                return isCardIDStrict(str);
+            }
+            return /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(str);
+        case 'pwd':     //密码以字母开头，长度在6~18之间，只能包含字母、数字和下划线
+            return /^[a-zA-Z]\w{5,17}$/.test(str)
+        case 'postal':  //邮政编码
+            return /[1-9]\d{5}(?!\d)/.test(str);
+        case 'QQ':      //QQ号
+            return /^[1-9][0-9]{4,9}$/.test(str);
+        case 'email':   //邮箱
+            return /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(str);
+        case 'money':   //金额(小数点2位)
+            return /^\d*(?:\.\d{0,2})?$/.test(str);
+        case 'URL':     //网址
+            return /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/.test(str)
+        case 'IP':      //IP
+            return /((?:(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d)\\.){3}(?:25[0-5]|2[0-4]\\d|[01]?\\d?\\d))/.test(str);
+        case 'date':    //日期时间
+            return /^(\d{4})\-(\d{2})\-(\d{2}) (\d{2})(?:\:\d{2}|:(\d{2}):(\d{2}))$/.test(str) || /^(\d{4})\-(\d{2})\-(\d{2})$/.test(str)
+        case 'number':  //数字
+            return /^[0-9]$/.test(str);
+        case 'english': //英文
+            return /^[a-zA-Z]+$/.test(str);
+        case 'chinese': //中文
+            return /^[\\u4E00-\\u9FA5]+$/.test(str);
+        case 'lower':   //小写
+            return /^[a-z]+$/.test(str);
+        case 'upper':   //大写
+            return /^[A-Z]+$/.test(str);
+        case 'HTML':    //HTML标记
+            return /<("[^"]*"|'[^']*'|[^'">])*>/.test(str);
+        default:
+            return true;
+    }
+}
+// 身份证严格校验
+const isCardIDStrict = (sId) => {
+    if (!/(^\d{15}$)|(^\d{17}(\d|X|x)$)/.test(sId)) {
+        console.log('你输入的身份证长度或格式错误')
+        return false
+    }
+    //身份证城市
+    var aCity = { 11: "北京", 12: "天津", 13: "河北", 14: "山西", 15: "内蒙古", 21: "辽宁", 22: "吉林", 23: "黑龙江", 31: "上海", 32: "江苏", 33: "浙江", 34: "安徽", 35: "福建", 36: "江西", 37: "山东", 41: "河南", 42: "湖北", 43: "湖南", 44: "广东", 45: "广西", 46: "海南", 50: "重庆", 51: "四川", 52: "贵州", 53: "云南", 54: "西藏", 61: "陕西", 62: "甘肃", 63: "青海", 64: "宁夏", 65: "新疆", 71: "台湾", 81: "香港", 82: "澳门", 91: "国外" };
+    if (!aCity[parseInt(sId.substr(0, 2))]) {
+        console.log('你的身份证地区非法')
+        return false
+    }
+
+    // 出生日期验证
+    var sBirthday = (sId.substr(6, 4) + "-" + Number(sId.substr(10, 2)) + "-" + Number(sId.substr(12, 2))).replace(/-/g, "/"),
+        d = new Date(sBirthday)
+    if (sBirthday != (d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate())) {
+        console.log('身份证上的出生日期非法')
+        return false
+    }
+
+    // 身份证号码校验
+    var sum = 0,
+        weights = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2],
+        codes = "10X98765432"
+    for (var i = 0; i < sId.length - 1; i++) {
+        sum += sId[i] * weights[i];
+    }
+    var last = codes[sum % 11]; //计算出来的最后一位身份证号码
+    if (sId[sId.length - 1] != last) {
+        console.log('你输入的身份证号非法')
+        return false
+    }
+
+    return true
+}
+// 数据类型校验
+const dataTypeList = ['String', 'Number', 'Boolean', 'Array', 'Function', 'Null', 'Undefined', 'Object', 'Date', 'RegExp', 'Error', 'Symbol', 'Promise', 'Set'];
+const isType = type => ctx => Object.prototype.toString.call(ctx) == `[object ${type}]`;
+let utilsType = {};
+dataTypeList.forEach(type => {
+    utilsType[`is${type}`] = isType(type);
+})
+// 获取滚动的坐标
+const getScrollPosition = (el = window) => ({
+    x: el.pageXOffset !== undefined ? el.pageXOffset : el.scrollLeft,
+    y: el.pageYOffset !== undefined ? el.pageYOffset : el.scrollTop
+});
+// 滚动到顶部
+const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, c - c / 8);
+    }
+}
+// el是否在视口范围内
+const elementIsVisibleInViewport = (el, partiallyVisible = false) => {
+    const { top, left, bottom, right } = el.getBoundingClientRect();
+    const { innerHeight, innerWidth } = window;
+    return partiallyVisible
+        ? ((top > 0 && top < innerHeight) || (bottom > 0 && bottom < innerHeight)) &&
+        ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth))
+        : top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
+}
+// 拦截粘贴板
+const copyTextToClipboard = (value) => {
+    var textArea = document.createElement("textarea");
+    textArea.style.background = 'transparent';
+    textArea.value = value;
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+        var successful = document.execCommand('copy');
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
+    document.body.removeChild(textArea);
+}
+// 防抖
+const debounce = (func, wait, immediate) => {
+    let timeout, args, context, timestamp, result;
+    const later = function later() {
+        // 距离上一次触发事件间隔
+        const last = +new Date() - timestamp;
+        if (last > 0 && last < wait) {
+            timeout = setTimeout(later, wait - last);
+        } else {
+            timeout = null
+            // 如果设定为immediate===true，因为开始边界已经调用过了此处无需调用
+            if (!immediate) {
+                result = func.apply(context, args)
+                if (!timeout) context = args = null
+            }
+        }
+    }
+    return function (...args) {
+        context = this
+        timestamp = +new Date()
+        const callNow = immediate && !timeout
+        // 如果延时不存在，重新设定延时
+        if (!timeout) timeout = setTimeout(later, wait)
+        if (callNow) {
+            result = func.apply(context, args)
+            context = args = null
+        }
+        return result
+    }
+}
+// 获取地址栏url拼接的数据
+const getQueryObject = (url) => {
+    url = url == null ? window.location.href : url
+    const search = url.substring(url.lastIndexOf('?') + 1)
+    const obj = {}
+    const reg = /([^?&=]+)=([^?&=]*)/g
+    search.replace(reg, (rs, $1, $2) => {
+        const name = decodeURIComponent($1)
+        let val = decodeURIComponent($2)
+        val = String(val)
+        obj[name] = val
+        return rs
+    })
+    return obj
+}
+
+export default {
+    checkStr,
+    isCardIDStrict,
+    getScrollPosition,
+    scrollToTop,
+    elementIsVisibleInViewport,
+    copyTextToClipboard,
+    debounce,
+    getQueryObject,
+    ...utilsType,
+}
